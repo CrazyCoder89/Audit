aba3e9487312ed64da71b26b03c2c045db919560
Krish Shah <krishshah089@gmail.com>
2026-05-15 20:14:25 +0530

    fix: stabilize postgres connection for Render + Neon

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,3 @@
-#sets up the database connection and session management using SQLAlchemy. 
-#creates engine and session factory
-#Every route that needs DB access will use get_db() to get a session
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -10,13 +7,26 @@ import os
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-#engine connection
-engine = create_engine(DATABASE_URL)
-#creates factory that created new db sessions for each request
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-#prent class for all models to inherit from
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,      # 👈 checks dead connections
+    pool_recycle=300,        # 👈 refresh connection every 5 min
+    pool_size=5,
+    max_overflow=10,
+    connect_args={
+        "sslmode": "require"  # 👈 VERY IMPORTANT for Neon
+    }
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
 Base = declarative_base()
-#fastapi routes call this to get a db session
+
 def get_db():
     db = SessionLocal()
     try:
